fe19c4d2aa5304f7a1ed37e7bda17bea26212f25
Vitaliy Alexandrovich <Dobrota2017@outlook.com>
2019-05-06 01:12:49 +0300

    SundayCommit

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -41,13 +41,37 @@ namespace SportsStore
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
-                    name: "pagination",
-                    template: "Products/Page/{page}/",
-                    defaults: new { Controller = "Product", action = "List" });
-
+                        name: null,
+                        template: "{category}/Page{page:int}",
+                        defaults: new { Controller = "Product", Action = "List" }
+                    );
                 routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Product}/{action=List}/{id?}");
+                        name: null,
+                        template: "Page{page:int}",
+                        defaults: new { Controller = "Product", Action = "List", page = 1 }
+                    );
+                routes.MapRoute(
+                        name: null,
+                        template: "{category}",
+                        defaults: new { Controller = "Product", Action = "List", page = 1 }
+                    );
+                routes.MapRoute(
+                        name: null,
+                        template: "",
+                        defaults: new { Controller = "Product", Action = "List", page = 1 }
+                    );
+                routes.MapRoute(
+                    name: null,
+                    template: "{controller}/{action}/{id?}");
+
+                //routes.MapRoute(
+                //    name: "pagination",
+                //    template: "Products/Page/{page}/",
+                //    defaults: new { Controller = "Product", action = "List" });
+
+                //routes.MapRoute(
+                //    name: "default",
+                //    template: "{controller=Product}/{action=List}/{id?}");
             });
             SeedData.EnsurePopulated(serviceProvider);
 
