5d79fb9d25f9c74a9a010d1b9cb7b1be3ab1d262
Vitaliy Alexandrovich <Dobrota2017@outlook.com>
2019-05-06 16:37:07 +0300

    MondayCommit

---
+++ SportsStore.Tests/NavigationMenuViewComponentTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Moq;
+using Xunit;
+using SportsStore.Components;
+using SportsStore.Models;
+
+namespace SportsStore.Tests
+{
+    public class NavigationMenuViewComponentTests
+    {
+        [Fact]
+        public void Can_Select_Categories()
+        {
+            //Organization
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+                new Product{ProductID = 1, Name = "P1", Category = "Apples"},
+                new Product{ProductID = 2, Name = "P2", Category = "Apples"},
+                new Product{ProductID = 3, Name = "P3", Category = "Plums"},
+                new Product{ProductID = 4, Name = "P4", Category = "Oranges"}
+            });
+            NavigationMenuViewComponent target = new NavigationMenuViewComponent(mock.Object);
+
+            //Action - getting a set of category
+            string[] results = ((IEnumerable<string>)(target.Invoke() as ViewViewComponentResult).ViewData.Model).ToArray();
+
+            //Statement
+            Assert.True(Enumerable.SequenceEqual(new string[] { "Apples", "Oranges", "Plums" }, results));
+        }
+    }
